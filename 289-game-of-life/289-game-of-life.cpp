f1bb0d806090bc2f0d8d0075273a34203b70395a
Adarsh Raj <48527325+adsv1623@users.noreply.github.com>
2022-04-12 10:39:49 +0530

    Time: 4 ms (26.80%), Space: 8.9 MB (5.20%) - LeetHub

--- 289-game-of-life/289-game-of-life.cpp
+++ 289-game-of-life/289-game-of-life.cpp
@@ -8,7 +8,8 @@ class Solution {
                 if(i==x && j==y) continue;
                 if(xi>=0 && yj>=0 && xi<n && yj<m ){
                     // safe
-                    ans+= board[xi][yj];
+                    if(board[xi][yj]!=2)
+                        ans+= board[xi][yj]==3?1: board[xi][yj];
                 }
             }
         }
@@ -17,32 +18,37 @@ class Solution {
 public:
     void gameOfLife(vector<vector<int>>& board) {
         int n = board.size(), m = board[0].size();
-        vector<vector<int>> arr(n, vector<int>(m,0));
-        for(int i=0;i<n;i++)
-            for(int j=0;j<m;j++) arr[i][j]= board[i][j];
-        
+       // 3: dead , 2:live 
         for(int i=0;i<n;i++){
             for(int j =0;j<m;j++){
                 if(board[i][j]==1){
                     // Jinda huin mai ;)
-                    int myNeigh = getSumSquare(arr,i,j);
+                    int myNeigh = getSumSquare(board,i,j);
                     if(myNeigh < 2){
-                        board[i][j] =0;
+                        board[i][j] =3;
                     }else if(myNeigh ==2 || myNeigh ==3){
                         continue;
                         // not clear
                     }else{
-                        board[i][j] =0;
+                        board[i][j] =3;
                     }
                     
                 }else{
                     // I'm dead ;)
-                    int myNeigh = getSumSquare(arr,i,j);
+                    int myNeigh = getSumSquare(board,i,j);
                     if(myNeigh==3){
-                        board[i][j]=1;
+                        board[i][j]=2;
                     }
                 }
             }
         }
+        
+        for(int i=0;i<n;i++){
+            for(int j =0;j<m;j++){
+                if(board[i][j]==3) board[i][j]=0;
+                else if(board[i][j]==2) board[i][j]=1;
+            }
+        }
+        
     }
 };
